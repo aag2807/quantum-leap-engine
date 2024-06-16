46454e66d2ffd6c5ff597134192ad6706ec39a03
Alvaro Guzman <alvaro.guzman@atl-software.net>
2024-06-16 15:47:49 -0400

    engine - project now finally compiles

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Quantum Leap Engine
 
-## IDEA
+## Idea
 The main goal is to create a game engine and a game to demonstrate it's abilities.
 It's supposed to be a type of engine similar to the hedgehog engine used to make sonic 06.
+
+## Made With
+This project is dependent on the following libraries
+* https://github.com/glfw/glfw.git
+* https://github.com/nigels-com/glew.git
+* https://github.com/bulletphysics/bullet3.git
+* https://github.com/ocornut/imgui.git
+
+If copying the project from scratch enter the lib directory and git clone all the projects into there.
+
--- main.cpp
+++ main.cpp
@@ -1,7 +1,99 @@
-#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include "imgui.h"
+#include "lib/imgui/backends/imgui_impl_glfw.h"
+#include "lib/imgui/backends/imgui_impl_opengl3.h"
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+void processInput(GLFWwindow *window);
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    // Initialize GLFW
+    if (!glfwInit())
+    {
+        return -1;
+    }
+
+    // Create a GLFW window
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "QuantumLeapEngine", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Initialize GLEW
+    if (glewInit() != GLEW_OK)
+    {
+        return -1;
+    }
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    (void) io;
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    // Setup Platform/Renderer bindings
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Input
+        processInput(window);
+
+        // Start the ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // Show a simple window
+        ImGui::Begin("Hello, ImGui!");
+        ImGui::Text("This is some text.");
+        ImGui::End();
+
+        // Render
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
